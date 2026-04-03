9fe0bf0f6853aa6e06f380bad9f19e9d7b21e257
MariaVCGarcia <120209017+MariaVCGarcia@users.noreply.github.com>
2026-04-03 12:20:45 -0400

    setting up all interactive ui on game screen

--- bonsai-tree-game/scripts/main_menu_Mouse_Cursor.tscn.gd
+++ bonsai-tree-game/scripts/main_menu_Mouse_Cursor.tscn.gd
@@ -7,12 +7,6 @@ const GAME_ASSETS__10 = preload("uid://c6n6egph8k6m7")
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(GAME_ASSETS__10,Input.CURSOR_POINTING_HAND)
 	
-func _process(_delta: float) -> void:
-	pass
-func on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	
-
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
